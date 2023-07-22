6e560720b3aa0084df3b3f4e7c9dc28a320446cd
AbdullohKomil <akomiljonov@icloud.com>
2023-07-22 12:47:23 +0500

    main vacancy edit

--- src/components/MainVacancys/MainVacancys.jsx
+++ src/components/MainVacancys/MainVacancys.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
+import { API } from '../../API/api';
 import CarCard from '../CarCard/CarCard';
 import { Header } from '../Header/Header';
 import './MainVacancys.scss';
+
 const MainVacancys = () => {
+  const { mutate } = useMutation('signup-users', API.signUp, {
+    onSuccess: (data) => {
+      if (data.data) {
+        setToken(data.data);
+
+        navigate('/');
+      }
+    },
+    onError: (err) => console.log(err),
+  });
+
+  const onSubmit = (values) => {
+    const formData = new FormData();
+
+    formData.append('firstName', values.firstName);
+    formData.append('lastName', values.lastName);
+    formData.append('phoneNumber', values.phoneNumber);
+    formData.append('region', values.region);
+    formData.append('passwordHash', values.passwordHash);
+
+    mutate(formData);
+  };
+
   return (
     <>
       <Header />
       <section className='main_vacancys'>
         <div className='container'>
           <div className='main_vacancys_inner'>
-            <CarCard />
-            <CarCard />
-            <CarCard />
-            <CarCard />
-            <CarCard />
             <CarCard />
           </div>
         </div>
